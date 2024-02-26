1d67bf3a08d7d60c8ec775a7dec9728b29e222e6
Israt-Jahan-Shonom <157152907+Israt-Jahan-Shonom@users.noreply.github.com>
2024-02-27 01:44:53 +0600

    Add files via upload

---
+++ session-1.js
@@ -0,0 +1,67 @@
+var a = "Sonom"
+var x = true
+var b = 10
+console.log("the data type of a variable is:", typeof(a))
+console.log("the data type of x variable is:", typeof(x))
+console.log("the data type of b variable is:", typeof(b))
+
+
+var c = 9
+var d = b+c
+console.log(d)
+var age = "180"
+console.log(age+d) //18019
+console.log(Number(age)+d) //199
+
+var e = 100
+var f = "th position"
+console.log(e+f)
+console.log(e.toString() + f)
+
+var firstname = "Israt Jahan"
+var lastname = "Shonom"
+var fullname = firstname+ " " + lastname
+console.log(fullname)
+
+// Assignment operator "="
+// Comparison operator "=="
+var a = "100"
+var b = 100
+var c = a===b
+//console.log(c)
+
+var fname1 = "alex"
+var fname2 = "jhon"
+//console.log(fname1==fname2)
+
+var t1 = true
+var f1 = true
+//console.log(f1==t1)
+
+// Not equal
+var var1 = 101
+var var2 = 100
+
+console.log(var1 <= var2)
+
+//2
+var a= 5
+var b = "60"
+var sum = a + Number(b)
+console.log(sum)
+
+//4
+var Num2 = 344
+var txtNum = "344"
+console.log(Num2===txtNum)
+
+//3
+var Num1 = 59
+var txt1 = "hello JS"
+console.log(Num1==txt1)
+
+//5
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
