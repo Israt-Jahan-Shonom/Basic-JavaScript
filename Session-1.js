076f1fc58a3823ef06512a92b92d922abe2270fa
Israt-Jahan-Shonom <157152907+Israt-Jahan-Shonom@users.noreply.github.com>
2024-02-27 01:59:22 +0600

    Update and rename session-1.js to Session-1.js

---
+++ Session-1.js
@@ -0,0 +1,40 @@
+/*1. Write code in JS: 
+Declare two variables, then assign your first name and last name. Finally, print your full name including one space between both names.
+*/
+var firstname = "Israt Jahan"
+var lastname = "Shonom"
+var fullname = firstname+ " " + lastname
+console.log(fullname)
+
+/*
+2. Write code in JS:    
+Suppose a = 5, b = "60". Use type conversion and print the sum of the two variables.
+*/
+var a= 5
+var b = "60"
+var sum = a + Number(b)
+console.log(sum)
+
+/*
+3. Write code in JS: 
+Num1 = 59, txt1 = "hello JS". Compare both variables and print the result.
+*/
+var Num1 = 59
+var txt1 = "hello JS"
+console.log(Num1==txt1)
+
+/*
+4. Write code in JS: 
+Num2 = 344, txtNum = "344". Compare the value of both variables along with the data type and print the result.
+*/
+var Num2 = 344
+var txtNum = "344"
+console.log(Num2===txtNum)
+
+/*
+5. Run the below code in JS, write your output:
+*/
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
